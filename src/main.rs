da25ecb9c5d5aa4df5feddf0c7bd9fb3369b1c0c
Ricardo Mendes <rokusu@gmail.com>
2020-12-11 10:46:11 +0000

    implement deploy for API docs and Guides
    This implements the logic necessary to deploy the
    API docs and Guides portions of an Ember.js release.
    It also configures GitHub to build a binary of the tool
    for Windows, macOS and Linux and upload it as an
    artifact.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,56 @@
+use std::str::FromStr;
+
+use structopt::StructOpt;
+mod projects {
+    pub mod api;
+    pub mod guides;
+}
+mod utils;
+
+#[derive(Debug, StructOpt)]
+enum Step {
+    Guides,
+    Api,
+}
+
+impl FromStr for Step {
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s.trim() {
+            "guides" => Ok(Step::Guides),
+            "api" => Ok(Step::Api),
+            other => {
+                println!("Step \"{}\" not recognized.", other);
+                std::process::exit(1);
+            }
+        }
+    }
+
+    type Err = std::string::ParseError;
+}
+
+#[derive(Debug, StructOpt)]
+struct Opts {
+    #[structopt(short, long)]
+    step: Option<Step>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut dir = tempfile::tempdir().unwrap().into_path();
+
+    let opts = Opts::from_args();
+
+    match opts.step {
+        Some(Step::Guides) => {
+            crate::projects::guides::deploy_guides(&mut dir).unwrap();
+        }
+        Some(Step::Api) => {
+            crate::projects::api::deploy_api_documentation(&mut dir).unwrap();
+        }
+        None => {
+            crate::projects::guides::deploy_guides(&mut dir).unwrap();
+            crate::projects::api::deploy_api_documentation(&mut dir).unwrap();
+        }
+    };
+
+    println!("Finished!");
 }
---
+++ src/projects/api.rs
@@ -0,0 +1,151 @@
+use git2::Repository;
+use process::ExitStatus;
+use std::{path::PathBuf, process};
+
+fn clone_repos(folder: &mut PathBuf) -> Result<Repository, git2::Error> {
+    println!("🤖 Cloning ember-jsonapi-docs");
+    folder.push("ember-jsonapi-docs");
+    Repository::clone(
+        "https://github.com/ember-learn/ember-jsonapi-docs.git",
+        &folder,
+    )?;
+    folder.pop();
+
+    println!("🤖 Cloning ember.js");
+    folder.push("ember.js");
+    Repository::clone("https://github.com/emberjs/ember.js.git", &folder)?;
+    folder.pop();
+
+    println!("🤖 Cloning data");
+    folder.push("data");
+    let repo = Repository::clone("https://github.com/emberjs/data.git", &folder)?;
+    folder.pop();
+
+    Ok(repo)
+}
+
+fn get_env_vars() -> Vec<(String, String)> {
+    println!("🤖 Fetching env vars from heroku");
+    let heroku_vars = std::process::Command::new("heroku")
+        // .current_dir(&dir)
+        .arg("config")
+        .arg("-s")
+        .args(&["-a", "api-viewer-json-docs-generator"])
+        .output()
+        .expect("Could not retrieve env vars.");
+    let str = String::from_utf8(heroku_vars.stdout.to_owned()).unwrap();
+
+    let mut res = vec![];
+    for line in str.trim().split("\n") {
+        let mut x = line.split("=").collect::<Vec<&str>>().into_iter();
+        res.push((x.next().unwrap().to_owned(), x.next().unwrap().to_owned()));
+    }
+
+    res
+}
+
+pub fn deploy_api_documentation(mut dir: &mut PathBuf) -> Result<ExitStatus, std::io::Error> {
+    println!("Beginning deploy for: API Documentation\n");
+
+    if cfg!(windows) {
+        check_heroku_cli_windows();
+    } else {
+        check_heroku_cli();
+    }
+    clone_repos(&mut dir).unwrap();
+
+    println!("Installing node dependencies");
+    dir.push("ember-jsonapi-docs");
+    process::Command::new("yarn")
+        .current_dir(&dir)
+        .spawn()
+        .expect("Could not install dependencies")
+        .wait()?;
+
+    println!("🤖 Generating API documentation…");
+    let vars = get_env_vars();
+    let result = process::Command::new("yarn")
+        .current_dir(&dir)
+        .envs(vars)
+        .args(&["run", "start", "--sync"])
+        .spawn()
+        .expect("Could not compile API documentation")
+        .wait();
+
+    std::fs::remove_dir_all(dir)?;
+    result
+}
+
+// Checks if heroku-cli is installed, and  then  checks if user is logged in.
+// I was getting bogged down on building up the command according to the platform, so...
+fn check_heroku_cli_windows() {
+    println!("👩‍💻 Checking heroku-cli");
+    match std::process::Command::new("cmd")
+        .args(&["/C", "heroku"])
+        .stdout(std::process::Stdio::null())
+        .status()
+    {
+        Ok(_) => {}
+        Err(_) => {
+            println!("heroku-cli not found. Please install and try again: https://devcenter.heroku.com/articles/heroku-cli");
+            std::process::exit(1);
+        }
+    };
+
+    match std::process::Command::new("cmd")
+        .args(&["/C", "heroku", "auth:whoami"])
+        .status()
+        .expect("Could not confirm login")
+        .success()
+    {
+        true => {}
+        false => {
+            let status = std::process::Command::new("cmd")
+                .args(&["/C", "heroku", "login"])
+                .spawn()
+                .expect("Could not log in user.")
+                .wait()
+                .expect("??");
+
+            if !status.success() {
+                std::process::exit(1);
+            }
+        }
+    };
+}
+
+// Checks if heroku-cli is installed, and  then  checks if user is logged in.
+fn check_heroku_cli() {
+    println!("👩‍💻 Checking heroku-cli");
+    match std::process::Command::new("heroku")
+        .stdout(std::process::Stdio::null())
+        .status()
+    {
+        Ok(_) => {}
+        Err(_) => {
+            println!("heroku-cli not found. Please install and try again: https://devcenter.heroku.com/articles/heroku-cli");
+            std::process::exit(1);
+        }
+    };
+
+    match std::process::Command::new("heroku")
+        .arg("auth:whoami")
+        .status()
+        .expect("Could not confirm login")
+        .success()
+    {
+        true => {}
+        false => {
+            let status = std::process::Command::new("heroku")
+                .arg("login")
+                .spawn()
+                .expect("Could not log in user.")
+                .wait()
+                .expect("??");
+
+            if !status.success() {
+                std::process::exit(1);
+            }
+        }
+    };
+}

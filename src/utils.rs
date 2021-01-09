39ddfee4f72e72a0d3e489005aebb10982ea8433
Ricardo Mendes <rokusu@gmail.com>
2021-01-09 01:53:41 +0000

    extract heroku env vars util

--- src/utils.rs
+++ src/utils.rs
@@ -47,3 +47,24 @@ pub fn prompt(task_type: TaskType, description: &str) {
     }
     print!("\n");
 }
+
+pub fn heroku_env_vars(project: &str) -> Vec<(String, String)> {
+    prompt(TaskType::Automated, "Fetching env vars from heroku");
+
+    let heroku_vars = std::process::Command::new("heroku")
+        // .current_dir(&dir)
+        .arg("config")
+        .arg("-s")
+        .args(&["-a", project])
+        .output()
+        .expect("Could not retrieve env vars.");
+    let str = String::from_utf8(heroku_vars.stdout.to_owned()).unwrap();
+
+    let mut res = vec![];
+    for line in str.trim().split("\n") {
+        let mut x = line.split("=").collect::<Vec<&str>>().into_iter();
+        res.push((x.next().unwrap().to_owned(), x.next().unwrap().to_owned()));
+    }
+
+    res
+}

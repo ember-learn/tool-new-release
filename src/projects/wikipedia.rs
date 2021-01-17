232109c213d37645f5dd7b319a2154e0e8e48172
Ricardo Mendes <rokusu@gmail.com>
2021-01-17 13:40:33 +0000

    improve wikipedia pipeline

--- src/projects/wikipedia.rs
+++ src/projects/wikipedia.rs
@@ -1,23 +1,71 @@
+use std::str::from_utf8;
+
+use chrono::format::StrftimeItems;
+use serde_json::Value;
 use utils::TaskType;
 
 use crate::utils::{self, prompt};
 
 pub fn run() {
+    let (stable_version, stable_date) = get_version_modified_pair("ember-source@latest");
+    let (preview_version, preview_date) = get_version_modified_pair("ember-source@beta");
+    let current_date = chrono::offset::Utc::now().date().format("%d %B %Y");
+
+    let infobox = format!(
+        "| latest release version = {stable_version}
+| latest release date    = {{{{Start date and age|{stable_date}}}}}<ref name=\"versions\">{{{{cite web|title=Releases · emberjs/ember.js|url=https://github.com/emberjs/ember.js/releases|website=[[GitHub]]|access-date={access_date}|language=en}}}}</ref>
+| latest preview version = {preview_version}
+| latest preview date    = {{{{Start date and age|{preview_date}}}}}<ref name=\"versions\" />",
+        stable_version = stable_version,
+        stable_date = stable_date,
+        preview_version = preview_version,
+        preview_date = preview_date,
+        access_date = current_date
+    );
+
     prompt(
         TaskType::Manual,
-        "Go to https://en.wikipedia.org/wiki/Ember.js",
+        "Go to https://en.wikipedia.org/w/index.php?title=Ember.js&action=edit.\nWe suggest you log in.",
     );
+
     prompt(
         TaskType::Manual,
-        "Click the Edit tab. We suggest you log in.",
+        format!(
+            "Replace the relevant release section of the Infobox with the following:\n\n{}\n",
+            infobox
+        )
+        .as_str(),
     );
+
     prompt(
         TaskType::Manual,
-        "Update the following in the Infobox:
-1. latest release version (including access-date)
-2. latest release date
-3. latest preview version
-4. latest preview date",
+        "Write a small summary and publish your changes!",
     );
-    prompt(TaskType::Manual, "Save!");
+}
+
+fn get_version_modified_pair(
+    package: &str,
+) -> (
+    semver::Version,
+    chrono::format::DelayedFormat<StrftimeItems>,
+) {
+    let output = std::process::Command::new("npm")
+        .args(&["show", package, "version"])
+        .output()
+        .expect("Could not find latest ember-source version.");
+    let version = from_utf8(&output.stdout).unwrap().trim();
+
+    let time_output = std::process::Command::new("npm")
+        .args(&["show", package, "time", "--json"])
+        .output()
+        .expect("Could not find latest ember-source version.");
+    let times_json = from_utf8(&time_output.stdout).unwrap();
+    let times: Value = serde_json::from_str(times_json).unwrap();
+    let modified_str = times[version].as_str().unwrap();
+    let modified_date = chrono::DateTime::parse_from_rfc3339(modified_str)
+        .unwrap()
+        .date();
+    let modified_formatted = modified_date.format("%Y|%m|%d");
+
+    (semver::Version::parse(version).unwrap(), modified_formatted)
 }

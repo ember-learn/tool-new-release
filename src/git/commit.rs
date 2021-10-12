966cd4ac9fb02099ffd2561c0f648f2f0f7204c8
Ricardo Mendes <rokusu@gmail.com>
2021-10-12 19:19:43 +0100

    run clippy

--- src/git/commit.rs
+++ src/git/commit.rs
@@ -1,5 +1,5 @@
 /// Creates a commit in the repository.
-pub fn commit(mut index: git2::Index, glitch_repo: &git2::Repository, version: &String) {
+pub fn commit(mut index: git2::Index, glitch_repo: &git2::Repository, version: &str) {
     let tree_id = index.write_tree().unwrap();
     let tree = glitch_repo.find_tree(tree_id).unwrap();
     let sig = glitch_repo.signature().unwrap();

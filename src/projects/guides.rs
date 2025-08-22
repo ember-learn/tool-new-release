1a90cb35469b1af4146c7d5fc294dd0e6a98f065
Ricardo Mendes <rokusu@gmail.com>
2025-08-22 23:41:31 +0100

    Format guides file

--- src/projects/guides.rs
+++ src/projects/guides.rs
@@ -1,21 +1,22 @@
 use crate::utils::prompt::{automated, manual};
-use std::{path::Path, process};
 use semver::{Version, VersionReq};
+use std::{path::Path, process};
 
 pub fn run(dir: &Path, dry_run: bool) {
     let npm_version_command = process::Command::new("node")
-            .arg("--version")
-            .output()
-            .expect("Could not check node version");
+        .arg("--version")
+        .output()
+        .expect("Could not check node version");
 
     let stdout_result = String::from_utf8(npm_version_command.stdout).unwrap();
-
     let req = VersionReq::parse(">=14.0.0").unwrap();
-
     let version = Version::parse(&stdout_result[1..stdout_result.len()].trim()).unwrap();
 
-    if  !req.matches(&version) {
-        panic!("Guides can only be installed with node version 14 and above right now. you have {:?}", stdout_result)
+    if !req.matches(&version) {
+        panic!(
+            "Guides can only be installed with node version 14 and above right now. you have {:?}",
+            stdout_result
+        )
     }
 
     manual("Check for pending PRs: https://github.com/ember-learn/guides-source/pulls");
@@ -35,7 +36,6 @@ pub fn run(dir: &Path, dry_run: bool) {
         if !status.success() {
             panic!("npm install failed");
         }
-
     }
 
     automated("Creating new version of guides");
